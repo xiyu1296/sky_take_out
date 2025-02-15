9e931222b4d825682ec796fde9b2145b3e54723f
xiyu1296 <2306881044@qq.com>
2025-02-15 10:03:42 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,11 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
     void insert(DishDTO dishDTO);
+
+    PageResult page(DishPageQueryDTO dishPageQueryDTO);
 }
